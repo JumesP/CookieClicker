7439b6bcbf18fd66520297cd6515b6d3905dd968
JumesP <118614403+JumesP@users.noreply.github.com>
2024-01-18 03:39:04 +0000

    progress more

--- v1/src/script.js
+++ v1/src/script.js
@@ -1,7 +1,19 @@
-let cookies = 1000000000
+let cookies = Math.round(parseFloat(localStorage.getItem("cookies") * 10000) / 10000)
+// let cookies = 0
 let multiplier = 1
 let increase = 1.25
 let num = 0
+let currentPageInvest = 0
+
+console.log(typeof parseFloat(localStorage.getItem("cookies")))
+console.log(localStorage.getItem("cookies"))
+console.log(cookies)
+
+// Statistics
+let totalEarnt = 0
+let totalCookieClicks = 0
+let totalFromAuto = 0
+let totalSpent = 0
 
 const amount_invested = [0, 0, 0, 0, 0, 0, 0]
 const investment_values = [0, 0.1, 1, 10, 100, 1000, 10000]
@@ -12,6 +24,11 @@ document.getElementById("multiplier").textContent = `${multiplier}x Multiplier`
 //basic sleeping function
 function sleep(ms) {
 	return new Promise(resolve => setTimeout(resolve, ms))
+}
+
+function spend(cost) {
+	cookies -= cost
+	totalSpent += cost
 }
 
 //update display
@@ -28,11 +45,22 @@ function updateStats() {
 		}
 	}
 
-	autoclicker = Math.floor(autoclicker * 100)/100
+	// displaying data
+	autoclicker = Math.floor(autoclicker * 100) / 100
 	document.getElementById("multiplier").textContent = `multiplier: ${multiplier}x`
 	document.getElementById("increase").textContent = `increase: ${increase}x`
 	document.getElementById("cookieCount").textContent = `${Math.floor(cookies)} cookie`
 	document.getElementById("autoclick").textContent = `${autoclicker} cookies per second`
+
+	document.getElementById("totalamount").textContent = `amount earnt total: $${totalEarnt}`
+	document.getElementById("totalclicks").textContent = `amount of clicks total: ${totalCookieClicks}`
+	document.getElementById("generatedauto").textContent = `amount earnt automatically: $${totalFromAuto}`
+	document.getElementById("spent").textContent = `amount spent: $${totalSpent}`
+
+	// storing data
+	localStorage.setItem("cookies", cookies.toString())
+
+	console.log("stats updated")
 }
 
 setInterval(function () {updateStats()}, 10)
@@ -42,6 +70,7 @@ function notEnoughMoney(button) {
 	var original = document.getElementById("investment1_button").textContent
 	document.getElementById(button).textContent = "Not Enough"
 	sleep(2000).then(() => document.getElementById(button).textContent = original)
+	// document.getElementById(button).textContent = original
 }
 
 function increaseCookies(num) {
@@ -52,7 +81,7 @@ function increaseCookies(num) {
 document.getElementById("cookie").onclick = function() {
 	cookies = Math.round((cookies+multiplier) * 10000) / 10000 // actual amount of cookies
 	document.getElementById("cookieCount").textContent = `${Math.floor(cookies)} cookie` // only show floor
-	return cookies
+	totalCookieClicks += 1
 }
 
 //increase multiplier per click
@@ -60,7 +89,7 @@ document.getElementById("investment1_button").onclick = function() {
 	if (cookies >= 20) {
 		multiplier = Math.round((multiplier *= increase) * 100) / 100
 		increase = ((increase - 1)/1.25)+1
-		cookies -= 20
+		spend(20)
 	} else {
 		notEnoughMoney("investment1_button")
 	}
@@ -73,7 +102,7 @@ document.getElementById("investment2_button").onclick = function() {
 			increaseCookies(num)
 		}, 1000)
 		amount_invested[num] += 1
-		cookies -= 200
+		spend(200)
 	} else {
 		notEnoughMoney("investment2_button")
 	}
@@ -140,5 +169,35 @@ document.getElementById("investment7_button").onclick = function() {
 	} else {
 		notEnoughMoney("investment7_button")
 	}
+}
+
+function displayEdit(input, change) {
+	document.getElementById(input).style.display = change
+}
+
+document.getElementById("next").onclick = function() {
+	let listOfScroll = ["investments", "statistics", "other"]
+
+	displayEdit(listOfScroll[currentPageInvest], "none")
+	currentPageInvest += 1
+	if (currentPageInvest > 2) {
+		currentPageInvest -= 3
+	}
+
+	document.getElementById("investments_header").textContent = listOfScroll[currentPageInvest][0].toUpperCase() + listOfScroll[currentPageInvest].slice(1)
+	displayEdit(listOfScroll[currentPageInvest], "flex")
+}
+
+document.getElementById("back").onclick = function() {
+	let listOfScroll = ["investments", "statistics", "other"]
+
+	displayEdit(listOfScroll[currentPageInvest], "none")
+	currentPageInvest -= 1
+	if (currentPageInvest < 0) {
+		currentPageInvest += 3
+	}
+
+	document.getElementById("investments_header").textContent = listOfScroll[currentPageInvest][0].toUpperCase() + listOfScroll[currentPageInvest].slice(1)
+	displayEdit(listOfScroll[currentPageInvest], "flex")
 }
 
